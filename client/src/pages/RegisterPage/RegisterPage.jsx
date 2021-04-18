2092555ec1134a93882d0207d69814428cea9492
MD-AZMAL <azmalmd12@gmail.com>
2021-04-18 22:46:16 +0530

    pages refactored

--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import LoginForm from "../../components/LoginForm/LoginForm";
 import PageSection from "../../components/PageSection/PageSection";
 import PageWrapper from "../../components/PageWrapper/PageWrapper";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
-const HomePage = () => {
+const RegisterPage = () => {
   return (
     <PageWrapper>
       <PageSection>
         <div className="text-center mb-5">
           <h2 className="text-color-major bold">Let's get started now!</h2>
           <p className="text-muted">
-            Or <Link to="/register">Create Account</Link> if not registered yet
+            Or <Link to="/">Login to your Account</Link> if you've already
+            registered
           </p>
         </div>
+
         <Container>
           <Row className="justify-content-center">
             <Col md="7">
-              <LoginForm />
+              <RegisterForm />
             </Col>
           </Row>
         </Container>
@@ -27,4 +29,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default RegisterPage;
